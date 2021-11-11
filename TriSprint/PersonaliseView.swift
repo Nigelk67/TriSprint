08fb04facc95d521ec9d52d43d6d121d02f4e12d
NigelKaran <nigel@mctsl.co.uk>
2021-11-11 16:57:34 +0100

    Added Personalise View

---
+++ TriSprint/PersonaliseView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonaliseView.swift
+//  TriSprint
+//
+//  Created by Nigel Karan on 11.11.21.
+//
+
+import SwiftUI
+
+struct PersonaliseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonaliseView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonaliseView()
+    }
+}
