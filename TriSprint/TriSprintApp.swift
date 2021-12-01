d7f86342e93705046b67f6e7ed5cd2bee670b59f
NigelKaran <nigel@mctsl.co.uk>
2021-12-01 17:11:47 +0100

    Firebase added

--- TriSprint/TriSprintApp.swift
+++ TriSprint/TriSprintApp.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TriSprintApp: App {
     let persistenceController = PersistenceController.shared
 
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             MainView()
